657e04ebd49fa2bb9ff28e1b39a0ccfc7b6547c0
luana-q-g <luana.garcia@catijr.com.br>
2021-07-29 12:25:39 -0300

    Adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import { router } from './routes'
 
@@ -9,6 +10,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
